35ce909ee8f5b77292ff88c5fd0c641cf19896a9
Ulises <ulises.aviles@cetys.edu.mx>
2022-05-11 16:34:18 -0700

    Added descriptions and imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+// React imports
+import { useState, useEffect } from "react";
 
+// NextJS imports
+import Head from "next/head";
+
+// Styles
+import styles from "../styles/pages/Home.module.css";
+
+// Compoents
+import Rule from "../components/rule";
+
+// Main component to export
 export default function Home() {
   return (
     <div className={styles.container}>
+      {/* Metadata */}
       <Head>
         <title>Forma normal de Chomsky</title>
         <meta
@@ -13,10 +24,28 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      {/* Content */}
       <main className={styles.main}>
+        {/* Title */}
         <h1 className={styles.title}>Forma normal de Chomsky</h1>
 
-        <p className={styles.description}>Hola</p>
+        {/* Description */}
+        <div className={styles.description}>
+          Una gramática de libre contexto está en su forma normal de Chomsky si
+          cada regla es de la forma
+          <br />
+          <Rule rule="A → BC" />
+          <br />
+          <Rule rule="A → α" />
+          <br />
+          Donde α es cualquier terminal y A, B y C son variables (donde B y C no
+          pueden ser la variable inicial).
+          <br />
+          Además, solo se permite la regla <Rule rule="S → ε" /> cuando S es la
+          variable inicial.
+        </div>
+
+        {/* Steps */}
       </main>
     </div>
   );
