2a8f8648971f2841e21e699c3413b3e6e314970c
Ulises <ulises.aviles@cetys.edu.mx>
2022-05-11 16:57:03 -0700

    Cleaned the code

--- components/rule.tsx
+++ components/rule.tsx
@@ -1,9 +1,12 @@
+// Import styles
 import styles from "../styles/components/Rule.module.css";
 
+// Define props interface
 interface props {
   rule: string;
 }
 
+// Component to export
 const rule = ({ rule }: props) => <span className={styles.rule}>{rule}</span>;
 
 export default rule;
