da2e0546096c5adcffc57c8851173ecdb4dc377f
Ulises <ulises.aviles@cetys.edu.mx>
2022-05-11 16:57:20 -0700

    Created youtube embed component

---
+++ components/YoutubeEmbed.tsx
@@ -0,0 +1,18 @@
+// Import styles
+import styles from "../styles/components/YoutubeEmbed.module.css";
+
+const YoutubeEmbed = ({ embedId }: { embedId: string }) => (
+  <div className={styles.video_responsive}>
+    <iframe
+      width="853"
+      height="480"
+      src={`https://www.youtube.com/embed/${embedId}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    />
+  </div>
+);
+
+export default YoutubeEmbed;
